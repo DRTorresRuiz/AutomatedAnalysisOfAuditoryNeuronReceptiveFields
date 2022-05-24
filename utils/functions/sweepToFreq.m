12b9c06aa431254ed6e478e258fe18992415032d
Daniel R Torres Ruiz <danielr.torresruiz@gmail.com>
2022-05-24 14:59:27 +0200

    [UPDATE] Documentation for sweepToFreq()

--- utils/functions/sweepToFreq.m
+++ utils/functions/sweepToFreq.m
@@ -1,6 +1,13 @@
 function y = sweepToFreq(y, sweeps, channels)
-%SWEEPTOFREQ Summary of this function goes here
-%   Detailed explanation goes here
+%SWEEPTOFREQ This function transforms the given list of sweep id number to the frequency value specified in the Trial information.
+% 
+% Usage example:
+% 
+% - Being `t` a Trial and `y` a list of sweep number for a specific spike:
+% 
+% >> `sweepToFreq(y, t.getSweeps(), t.Channels)`
+%
+% $Author: DRTorresRuiz$
     for i = 1:length(y)
         y(i) = sweeps( y(i) * channels - (channels - 1) ).CarFreq;
     end
