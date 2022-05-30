fcdbf1c6d08b1684db98c5391d2012047e9ccfad
Daniel R Torres Ruiz <danielr.torresruiz@gmail.com>
2022-05-30 19:22:36 +0200

    [MINOR UPDATE] Change variables names of readingTrials example.

--- utils/tests/readingTrials.m
+++ utils/tests/readingTrials.m
@@ -1,15 +1,18 @@
 % $Author: DRTorresRuiz$
+close all
+clear all
+
 %% SET UP VARIABLES
 clc
 channels = 2;
 
 %% FIND FILES
 fprintf("Reading files...\n");
-f = filesForNeuron(".\IC Ionto\13_128_Ionto\FRA\", 13, 128, [1, 2]);
+file = filesForNeuron(".\IC Ionto\13_128_Ionto\FRA\", 13, 128, [1, 2, 3, 5, 6, 7]);
 
 %% GET TRIAL INFORMATIONS
 fprintf("Getting trial information from files...\n")
-trials = getTrials(f, channels);
+trials = getTrials(file, channels);
 clc
 fprintf("Trial information loaded successfully.\n\n\ttrials:\n");
 disp(trials)
