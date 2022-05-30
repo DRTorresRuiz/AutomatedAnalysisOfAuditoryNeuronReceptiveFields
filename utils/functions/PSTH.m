3f8b405b889d8079b66a4e64ba8b2860e99031f9
Daniel R Torres Ruiz <danielr.torresruiz@gmail.com>
2022-05-30 20:50:47 +0200

    [CREATE] Plot a PSTH for a given set of times

---
+++ utils/functions/PSTH.m
@@ -0,0 +1,21 @@
+function [values, h] = PSTH( x, edges )
+%PSTH Plot a PSTH.
+%
+% Usage example:
+%
+% >> PSTH( x, x_values )
+%
+% $Author: DRTorresRuiz$
+arguments
+    x
+    edges = []
+end
+    if isempty(edges)
+        edges = 0:max(x);
+    end
+    
+    h = histogram( x, edges );
+    values = h.Values;
+    ylabel( "spikes/ms" );
+end
+
